64e409a20c9ceafedccaa69cafbb236c5d7827ae
Khushnor Rahman Meem <khushnorrahmanmeem@gmail.com>
2024-05-04 20:29:11 +0600

    Model 14.2

---
+++ project_7/seventh_project/seventh_project/first_app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from first_app.models import StudentModel
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentModel
+        fields='__all__'
+        labels={
+            'name': 'Student Name',
+            'roll': 'Student Roll'
+        }
+        widgets={
+            'name':forms.TextInput()
+        }
+        help_texts={
+            'name': 'Enter Student Name',
+            'roll': 'Enter Student Roll'
+        }
+        error_messages={
+            'name': {'required': "name is required"}
+              
+            },
+           
+
