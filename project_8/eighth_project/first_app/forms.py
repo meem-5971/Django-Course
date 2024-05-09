ea8ed2b0ffaa1fc8b3bc39b09559c00557ab645c
Khushnor Rahman Meem <khushnorrahmanmeem@gmail.com>
2024-05-10 01:48:05 +0600

    Module 17

---
+++ project_8/eighth_project/first_app/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django import forms
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','first_name','last_name','email']
+
+class ChangeUserData(UserChangeForm):
+    password = None
+    class Meta:
+        model=User
+        fields=['username','first_name','last_name','email']
